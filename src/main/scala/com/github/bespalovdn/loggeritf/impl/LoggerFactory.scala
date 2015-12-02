fa45f95c4798c9baabbba090d04c15128bc57118
Dmitry Bespalov <bespalovdn@gmail.com>
2015-12-02 21:24:25 +0300

    BufferLoggerFactory intro

--- src/main/scala/com/github/bespalovdn/loggeritf/impl/LoggerFactory.scala
+++ src/main/scala/com/github/bespalovdn/loggeritf/impl/LoggerFactory.scala
@@ -1,8 +1,20 @@
 package com.github.bespalovdn.loggeritf.impl
 
+import java.util.concurrent.atomic.AtomicReference
+
 import com.github.bespalovdn.loggeritf.Logger
+import com.github.bespalovdn.loggeritf.impl.factory.BufferLoggerFactory
 
 trait LoggerFactory
 {
     def newLogger(clazz: Class[_], mdc: Map[String, String] = null): Logger
+    def update(newFactory: LoggerFactory): Unit = {}
+}
+
+object LoggerFactory
+{
+    def apply(): LoggerFactory = factory.get()
+    def update(f: LoggerFactory): Unit = factory.set(f)
+
+    private val factory: AtomicReference[LoggerFactory] = new AtomicReference(BufferLoggerFactory)
 }
